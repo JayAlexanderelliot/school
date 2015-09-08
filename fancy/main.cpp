cd6e0f07fd716552b463503e0b9c0873e01d7c82
jay-alexander elliot <jalexander.elliot@yahoo.comcd>
2015-09-07 22:56:55 -0700

    add fancy Please enter the commit message for your changes. Lines starting

---
+++ fancy/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string name;
+    int age;
+    string Lname;
+    cout << "Jay" << endl;
+    cin >> name;
+    cout << "Elliot" << endl;
+    cin >> Lname;
+    cout << "21" << endl;
+    cin >> age;
+}
